b72250066e8f6edf4f2494f0738bad5eae7faed2
DevMarc <seu-email@example.com>
2024-10-31 13:08:35 -0400

    Adicionando pastas main e test

---
+++ main/java/br/com/rpires/dao/IClienteDAO.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.com.rpires.dao;
+
+import br.com.rpires.dao.generic.IGenericDAO;
+import br.com.rpires.domain.Cliente;
+
+/**
+ * @author FuturoDevMarcos
+ *
+ */
+public interface IClienteDAO extends IGenericDAO<Cliente, Long> {
+
+
+}
